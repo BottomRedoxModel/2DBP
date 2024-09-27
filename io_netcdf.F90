b99d666834a4eea5a99d47dc5d86a752a6426020
Beatriz Arellano <barellano@protonmail.com>
2024-09-27 16:45:00 +0100

    Fixes the thickness of the surface layer

--- io_netcdf.F90
+++ io_netcdf.F90
@@ -298,10 +298,11 @@
       dz_w(1:k_wat_bbl-1) = z_w(2:k_wat_bbl) - z_w(1:k_wat_bbl-1)
       dz_w(k_wat_bbl) = 0.0_rk
       !Layer thicknesses (hz_w)
-      hz_w(1) = dz_w(1)
+      hz_w(1) = z_w(1) + 0.5_rk*dz_w(1)
       do j=2,k_wat_bbl
           hz_w(j) = 0.5_rk*(dz_w(j-1)+dz_w(j))
       end do
+
       !Set the water temperature (t_w), salinity (s_w), vertical diffusivity (kz_w),
       !and (if required) the surface irradiance (Eair) and ice thickness (hice)
     !  do istep=1,steps_in_yr-1 !Loop over hours
